361a586991da188fca6ecf1d622a4bba38598a53
Alexandre Asselin <alexandre.asselin@gsoft.com>
2023-02-28 19:43:02 -0500

    duplicate all files

---
+++ level_two/shoppies_react/src/component/Movie/nominationList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./styles/nominationList.module.css";
+import Button from "../UI/Button";
+
+const NominationList = (props) => {
+  const onRemove = () => {
+    props.onRemove(props);
+  };
+
+  return (
+    <div className={styles.nominationList}>
+      {props.title} ({props.year})<Button onClick={onRemove}>Remove</Button>
+    </div>
+  );
+};
+
+export default NominationList;
---
+++ level_two/shoppies_react/src/component/Movie/result_And_nomination.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import ResultBar from "./resultBar";
+import NominationBar from "./nominationBar";
+import styles from "./styles/result_And_nomination.module.css";
+
+const Result_And_nomination = (props) => {
+  let savedNomination = JSON.parse(localStorage.getItem("Nomination"));
+  let nomination = [];
+  if (savedNomination.length !== 0) {
+    nomination = savedNomination;
+  }
+
+  const [nominationList, setNominationList] = useState(nomination);
+  const [resultList, setResultList] = useState(props.itemsList);
+
+  useEffect(() => {
+    localStorage.setItem("Nomination", JSON.stringify(nominationList));
+  }, [nominationList]);
+
+  const nominationHandler = (newData) => {
+    const filteredResult = resultList.filter((element) => {
+      return newData.id !== element.id;
+    });
+
+    setResultList(filteredResult);
+
+    setNominationList((prevData) => {
+      return [newData, ...prevData];
+    });
+  };
+
+  const removeHandler = (data) => {
+    const filteredList = nominationList.filter((element) => {
+      return element.id !== data.id;
+    });
+    setNominationList(() => {
+      return filteredList;
+    });
+
+    setResultList((prevData) => {
+      return [...prevData, data];
+    });
+  };
+
+  return (
+    <div className={styles.result_And_nomination}>
+      <ResultBar
+        itemsList={resultList}
+        onNominate={nominationHandler}
+        movieName={props.movieName}
+      ></ResultBar>
+      <NominationBar
+        nominationList={nominationList}
+        onRemove={removeHandler}
+      ></NominationBar>
+    </div>
+  );
+};
+
+export default Result_And_nomination;
